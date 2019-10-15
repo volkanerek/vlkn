5d5c22d528c45e109e8753664677b3ceb1d43720
volkanerek <56043943+volkanerek@users.noreply.github.com>
2019-10-15 22:04:40 +0300

    Add files via upload

---
+++ HW#3.py
@@ -0,0 +1,14 @@
+age = int ( input("Please enter your age!") )
+x = 19
+y = 10
+z = 1
+if age > x:
+    print("You are an adult!")
+elif age <= x and age > y:
+    print("You are an adolescent!")
+elif age <= y and age >= z:
+    print("You are a child!")
+else:
+    print("You are an infant!")
+    
+#According to guidance you have done at the class, ı have removed the float input part from homework because it was out of definition of given task. 
